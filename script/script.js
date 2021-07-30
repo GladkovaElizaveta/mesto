42506c608590d6f60ecf72d4305c898803e1b6ac
GladkovaElizaveta <84663547+GladkovaElizaveta@users.noreply.github.com>
2021-07-30 11:02:01 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,28 @@
+let page = document.querySelector('.page');
+let profileEditButton = page.querySelector('.profile__edit-button');
+let profileName = page.querySelector('.profile__name');
+let profileDescription = page.querySelector ('.profile__description');
+let popup = page.querySelector('.popup');
+let popupCloseButton = page.querySelector('.popup__container-exit-button');
+let formElement = page.querySelector('.popup__profile-form')
+let nameInput = page.querySelector('.popup__profile-input_type_name');
+let jobInput = page.querySelector('.popup__profile-input_type_description');
+
+let openPopup = function () {
+    popup.classList.add('popup_opened')
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+}
+
+let closePopup = function () {popup.classList.remove('popup_opened')};
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    closePopup();
+}
+
+profileEditButton.addEventListener('click', openPopup);
+formElement.addEventListener('submit', formSubmitHandler); 
+popupCloseButton.addEventListener('click', closePopup);
