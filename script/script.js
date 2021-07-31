5e2b7a134770aad8784cf25d89a333c9c950ccf0
GladkovaElizaveta <84663547+GladkovaElizaveta@users.noreply.github.com>
2021-08-01 01:54:55 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,28 +1,31 @@
 let page = document.querySelector('.page');
-let profileEditButton = page.querySelector('.profile__edit-button');
+
 let profileName = page.querySelector('.profile__name');
 let profileDescription = page.querySelector ('.profile__description');
+
+let profileEditButton = page.querySelector('.profile__edit-button');
+
 let popup = page.querySelector('.popup');
 let popupCloseButton = page.querySelector('.popup__container-exit-button');
-let formElement = page.querySelector('.popup__profile-form')
-let nameInput = page.querySelector('.popup__profile-input_type_name');
-let jobInput = page.querySelector('.popup__profile-input_type_description');
+let cardElement = page.querySelector('.popup__profile-card')
+let nameInput = page.querySelector('.popup__profile-input_name');
+let activityInput = page.querySelector('.popup__profile-input_description');
 
-let openPopup = function () {
+function openPopup() {
     popup.classList.add('popup_opened')
     nameInput.value = profileName.textContent;
-    jobInput.value = profileDescription.textContent;
+    activityInput.value = profileDescription.textContent;
 }
 
-let closePopup = function () {popup.classList.remove('popup_opened')};
+function closePopup() {popup.classList.remove('popup_opened')};
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
+    profileDescription.textContent = activityInput.value;
     closePopup();
 }
 
 profileEditButton.addEventListener('click', openPopup);
-formElement.addEventListener('submit', formSubmitHandler); 
 popupCloseButton.addEventListener('click', closePopup);
+cardElement.addEventListener('submit', formSubmitHandler); 
